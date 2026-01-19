6daeccbb654ae99d2aed9a5c5b2ff22611b132d4
jony <ravshanovjony7@gmail.com>
2026-01-20 00:35:00 +0500

    the end amaliyot

--- assets/app.js
+++ assets/app.js
@@ -91,7 +91,7 @@
 
 // // // // // // /// //// //// 6 DARS MASALA // /// // /// // // /// /// // / //
 
-const numberOfSeries = +prompt("Nechta seria kordingiz?", "");
+const numberOfSeries = +prompt("Nechta serial kordingiz?", "");
 
 const seriesDB = {
   count: numberOfSeries,
@@ -101,17 +101,23 @@ const seriesDB = {
   privat: false,
 };
 
-for (let i = 1; i <= 2; i++) {
-  const a = prompt("Oxirgi korgan serialingiz?", "");
-  const b = prompt("Nechi baho berasiz?", "");
-
-  if (a === null) {
-    i = 1;
-  } else {
+for (let i = 0; i < 2; i++) {
+  const a = prompt(`Oxirgi korgan serialingiz ${i}?`, "");
+  const b = prompt(`Nechi baho berasiz ${i}?`, "");
+  if (a !== null && a !== "" && b !== null && b !== "") {
     seriesDB.series[a] = b;
+  } else {
+    i--;
   }
 }
 
+if (seriesDB.count < 5) {
+  console.log("Siz kam serial koripsiz");
+} else if (seriesDB.count >= 5 && seriesDB.count < 9) {
+  console.log("Siz classik tomoshabil");
+} else if (seriesDB.count >= 10) {
+  console.log("Siz serialchi zvezda ekan siz");
+}
 console.log(seriesDB);
 
 // const a = prompt("Oxirgi korgan serialingiz?", "")
