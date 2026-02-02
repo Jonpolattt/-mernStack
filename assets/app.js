08da4f25ed3f39b97a43f665929f02083abb284d
jony <ravshanovjony7@gmail.com>
2026-02-02 19:04:04 +0500

    11. amaliyot

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
-const numberOfSeries = +prompt("Nechta serial kordingiz?", "");
+let numberOfSeries;
+
+startApp();
 
 const seriesDB = {
   count: numberOfSeries,
@@ -8,25 +10,54 @@ const seriesDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt(`Oxirgi korgan serialingiz ${i}?`, "");
-  const b = prompt(`Nechi baho berasiz ${i}?`, "");
-  if (a !== null && a !== "" && b !== null && b !== "") {
-    seriesDB.series[a] = b;
-  } else {
-    i--;
+setFavoriteSeries();
+detectingLevel();
+writeGenres();
+showDb(seriesDB.privat);
+
+function startApp() {
+  while (
+    numberOfSeries == "" ||
+    numberOfSeries == null ||
+    isNaN(numberOfSeries)
+  ) {
+    numberOfSeries = +prompt("Nechta serial kordingiz?", "");
   }
 }
 
-if (seriesDB.count < 5) {
-  console.log("Siz kam serial koripsiz");
-} else if (seriesDB.count >= 5 && seriesDB.count < 9) {
-  console.log("Siz classik tomoshabil");
-} else if (seriesDB.count >= 10) {
-  console.log("Siz serialchi zvezda ekan siz");
+function setFavoriteSeries() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt(`Oxirgi korgan serialingiz ${i}?`, "");
+    const b = prompt(`Nechi baho berasiz ${i}?`, "");
+    if (a !== null && a !== "" && b !== null && b !== "") {
+      seriesDB.series[a] = b;
+    } else {
+      i--;
+    }
+  }
 }
-console.log(seriesDB);
 
+function detectingLevel() {
+  if (seriesDB.count < 5) {
+    console.log("Siz kam serial koripsiz");
+  } else if (seriesDB.count >= 5 && seriesDB.count < 9) {
+    console.log("Siz classik tomoshabil");
+  } else if (seriesDB.count >= 10) {
+    console.log("Siz serialchi zvezda ekan siz");
+  }
+}
 
-console.log("uzimdi uzim aldab push qilip yotipman");
+function writeGenres() {
+  for (let i = 1; i < 4; i++) {
+    const genre = prompt(`Yaxshi ko'rgan janiringiz ${i}?`, "");
+    seriesDB.geners[i] = genre;
+  }
+}
 
+function showDb(isPrivat) {
+  if (!isPrivat) {
+    console.log(seriesDB);
+  } else {
+    console.log("Siz bu yerga kiraolmeysiz");
+  }
+}
