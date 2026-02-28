17148e5ddd4f3c82f68f56812228f2ab15d47a34
jony <ravshanovjony7@gmail.com>
2026-02-28 15:36:01 +0500

    hodisa amaliyot

--- assets/amaliyot.js
+++ assets/amaliyot.js
@@ -1,33 +1,75 @@
-const advs = document.querySelectorAll('.promo__adv img'),
-	genre = document.querySelector('.promo__genre'),
-	wrapper = document.querySelector('.promo__bg'),
-	seriesList = document.querySelector('.promo__interactive-list')
+document.addEventListener("DOMContentLoaded", function () {
+  const advs = document.querySelectorAll(".promo__adv img"),
+    genre = document.querySelector(".promo__genre"),
+    wrapper = document.querySelector(".promo__bg"),
+    seriesList = document.querySelector(".promo__interactive-list"),
+    input = document.querySelector(".adding__input"),
+    addForm = document.querySelector("form.add"),
+    checkbox = addForm.querySelector("[type='checkbox']");
 
-const seriesDB = {
-	series: [
-		'Omar',
-		'The Final Legacy',
-		'Ertugrul',
-		'Magnificent Century',
-		'The Great Seljuks: Guardians...',
-	],
-}
+  const seriesDB = {
+    series: [
+      "Omar",
+      "The Final Legacy",
+      "Ertugrul",
+      "Magnificent Century",
+      "The Great Seljuks: Guardians...",
+    ],
+  };
 
-advs.forEach(item => {
-	item.remove()
-})
+  addForm.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-genre.textContent = 'Comedy'
+    let newValue = input.value;
+    const favourite = checkbox.checked;
 
-wrapper.style.backgroundImage = 'url(./img/2.jpg)'
+    if (newValue) {
+      if (newValue.length > 18) {
+        newValue = `${newValue.slice(0, 18)} ...`;
+      }
+      if (favourite) {
+        console.log("Sevimli serialingiz qoshildi");
+      }
+      seriesDB.series.push(newValue);
+      sortSeries(seriesDB.series);
+      setList();
+      e.target.reset();
+      // input.value = "";
+    }
+  });
 
-seriesList.innerHTML = ''
+  advs.forEach((item) => {
+    item.remove();
+  });
+  genre.textContent = "Comedy";
 
-seriesDB.series.forEach((item, index) => {
-	seriesList.innerHTML += `
-  <li class="promo__interactive-item">
-    ${index + 1}. ${item}
-    <div class="delete"></div>
-  </li>
-  `
-})
+  // wrapper.style.backgroundImage = "url(./img/2.jpg)";
+
+  function sortSeries(arr) {
+    arr.sort();
+  }
+
+  function setList() {
+    seriesList.innerHTML = "";
+    sortSeries(seriesDB.series);
+    seriesDB.series.forEach((item, index) => {
+      seriesList.innerHTML += `
+        <li class="promo__interactive-item">
+          ${index + 1}. ${item}
+          <div class="delete"></div>
+        </li>
+      `;
+      document.querySelectorAll(".delete").forEach((item, index) => {
+        item.addEventListener("click", () => {
+          item.parentElement.remove();
+          seriesDB.series.splice(index, 1);
+
+          setList();
+        });
+      });
+    });
+  }
+
+  sortSeries(seriesDB.series);
+  setList();
+});
