20671e52e7942a1e907530a28d2bd327fc3f3ef3
jony <ravshanovjony7@gmail.com>
2026-03-25 20:27:27 +0500

    OOP

---
+++ assets/samar.js
@@ -0,0 +1,15 @@
+// OOP MODULI
+
+// 15 OOP
+class Car {
+  constructor(color, brand, maxSpeed) {
+    color = this.color;
+    brand = this.brand;
+    maxSpeed = this.maxSpeed;
+  }
+
+  speed() {
+    console.log("Joha");
+  }
+}
+
