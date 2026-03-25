31ae668fcf225e30fc8b8707cb759ff006632f9d
jony <ravshanovjony7@gmail.com>
2026-03-25 21:14:28 +0500

    Consturctor function

--- assets/samar.js
+++ assets/samar.js
@@ -14,38 +14,55 @@
 // }
 
 // #16. Js OOP
-const person = {
-  name: "Jonpo'lat",
-  introduce() {
-    console.log(`My name is ${this.name}`);
-  },
-};
+// const person = {
+//   name: "Jonpo'lat",
+//   introduce() {
+//     console.log(`My name is ${this.name}`);
+//   },
+// };
 
-console.log(person);
-console.log(person.toString());
+// console.log(person);
+// console.log(person.toString());
 
-// # Prototype ni 3 xil yaaratish ulsuli:
+// // # Prototype ni 3 xil yaaratish ulsuli:
 
-// #1 Consturctor Function
-const Car = function (brand, color) {};
+// // #1 Consturctor Function
+// const Car = function (brand, color) {};
 
-// #2 ES6 Classes
-class Car2 {
-  constructor(color, brand, maxSpeed) {
-    this.color = color;
-    this.brand = brand;
-    this.maxSpeed = maxSpeed;
-  }
-}
+// // #2 ES6 Classes
+// class Car2 {
+//   constructor(color, brand, maxSpeed) {
+//     this.color = color;
+//     this.brand = brand;
+//     this.maxSpeed = maxSpeed;
+//   }
+// }
 
-// #3 Object create
+// // #3 Object create
 
-const CarProte = {
-  init(brand, color) {
-    this.color = color;
-    this.brand = brand;
-  },
-};
+// const CarProte = {
+//   init(brand, color) {
+//     this.color = color;
+//     this.brand = brand;
+//   },
+// };
 
-const porshe = Object.create(CarProte);
-porshe.init("porshe", "black");
+// const porshe = Object.create(CarProte);
+// porshe.init("porshe", "black");
+
+// // #17 CONSTRUCTOR FUNCTION
+// function Car(brand, color) {
+//   //   console.log(this); // Bosh object {}
+//   this.brand = brand;
+//   this.color = color;
+
+//   // Х Tavsiya etilmaydi xar bir objectga clone buladi
+//   this.startEngine = function () {
+//     console.log(`Engine starting... + ${brand}`);
+//   };
+// }
+
+// const porshe = new Car("porshe", "yellow");
+// const merc = new Car("mercedes", "white");
+// console.log(porshe);
+// console.log(merc);
